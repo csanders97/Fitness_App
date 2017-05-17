84c0249961d51ca8b961061cb727ff42abaf3259
zpeterson22 <zachrp2296@yahoo.com>
2017-05-17 14:20:17 -0600

    added a way to get user info by id

--- GetFit/app/src/main/java/com/example/csanders/getfit/Views/Profile.java
+++ GetFit/app/src/main/java/com/example/csanders/getfit/Views/Profile.java
@@ -3,16 +3,28 @@ package com.example.csanders.getfit.Views;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.example.csanders.getfit.MainActivity;
+import com.example.csanders.getfit.Models.Users;
 import com.example.csanders.getfit.R;
+import com.example.csanders.getfit.Tables.DBHandler;
+
+import java.util.List;
 
 /**
  * Created by Sophie on 5/15/2017.
  */
 
 public class Profile extends Activity {
+    DBHandler db = new DBHandler(this);
+    List<Users> users = db.getAllUsers();
+    MainActivity main = new MainActivity();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
     }
+
+
 }
