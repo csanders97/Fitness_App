e7108fa1803acfa5f4b1359be1dabaf3b68d6f40
Caleb Sanders <colonelsanders217@gmail.com>
2017-05-17 10:00:24 -0600

    Made changes to "authentication" in the main activity class

--- GetFit/app/src/main/java/com/example/csanders/getfit/MainActivity.java
+++ GetFit/app/src/main/java/com/example/csanders/getfit/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.csanders.getfit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +10,7 @@ import android.widget.EditText;
 
 import com.example.csanders.getfit.Models.Users;
 import com.example.csanders.getfit.Tables.DBHandler;
+import com.example.csanders.getfit.Views.Home;
 
 import java.util.List;
 
@@ -17,7 +19,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button logClick;
     private EditText userName;
     private EditText pass;
-    private String user;
+    private String name;
     private String password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,30 +29,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         regClick.setOnClickListener(this);
         logClick = (Button) findViewById(R.id.loginbtn);
         logClick.setOnClickListener(this);
-        userName = (EditText) findViewById(R.id.usernameinput);
-        user = userName.getText().toString();
         pass = (EditText) findViewById(R.id.passwordinput);
         password = pass.getText().toString();
-        DBHandler db = new DBHandler(this);
-
-// Inserting Shop/Rows
-        Log.d("Insert: ", "Inserting ..");
-        db.addUser(new Users("Billy ", "Bob ", 300 , 6 ));
-
-// Reading all shops
-        Log.d("Reading: ", "Reading all shops..");
-        List<Users> users = db.getAllShops();
-
-        for (Users user : users) {
-            String log = "Id: " + user.getUserId() + ",FirstName: " + user.getFirstName() + ",LastName: " + user.getLastName() + ",Weight: " + user.getWeight() + ",Height: " + user.getHeight();
-// Writing shops to log
-            Log.d("Users: : ", log);
-        }
     }
 
+    public void onClick(View view) {
+        userName = (EditText) findViewById(R.id.usernameinput);
+        name = userName.getText().toString();
+        DBHandler db = new DBHandler(this);
+        boolean doesExist = false;
+        //db.addUser(new Users("John", "Doe", 300, 67));
+        List<Users> users = db.getAllShops();
+        for (Users user : users) {
+            Log.i("Name: ", user.getFirstName());
+            Log.i("Input: ", name);
+            Log.i("Bool: ", Boolean.toString(doesExist));
+            if (user.getFirstName().equals(name)) {
+                doesExist = true;
+                Log.i("Bool: ", Boolean.toString(doesExist));
+            }
+        }
+        switch(view.getId()) {
+            case R.id.registerbtn:
+                //logic
+                break;
+            case R.id.loginbtn:
+                //logic
+                if (doesExist) {
+                    Log.i("Login: ", "Welcome back");
+                }
+                else {
+                    Log.i("Login: ", "You need to register this user");
+                }
+                  Intent intent = new Intent(this, Home.class);
+                  startActivity(intent);
+                break;
 
-    @Override
-    public void onClick(View v) {
-
+        }
     }
 }
