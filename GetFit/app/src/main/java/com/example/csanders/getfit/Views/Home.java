72e518101b95b9dc572b9776ac98cc91f7225dbf
Caleb Sanders <colonelsanders217@gmail.com>
2017-05-17 11:49:09 -0600

    Progress on the home page

--- GetFit/app/src/main/java/com/example/csanders/getfit/Views/Home.java
+++ GetFit/app/src/main/java/com/example/csanders/getfit/Views/Home.java
@@ -11,16 +11,27 @@ import com.example.csanders.getfit.R;
  * Created by Matthew Balderas on 5/15/2017.
  */
 
-public class Home extends Activity {
+public class Home extends Activity implements View.OnClickListener {
     private Button prof;
     private Button lib;
-    private Button work;
+    private Button search;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        prof = (Button) findViewById(R.id.profileBtn);
+        lib = (Button) findViewById(R.id.libraryBtn);
+        search = (Button) findViewById(R.id.searchBtn);
+        prof.setOnClickListener(this);
+        lib.setOnClickListener(this);
+        search.setOnClickListener(this);
+    }
 
-
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            //case r.id.pro
+        }
     }
 }
 
