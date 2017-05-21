ec3a62820f88b181d424a333253eca9e1d6bd864
MBalderasNu <mexico2067@gmail.com>
2017-05-20 18:23:18 -0600

    Add code to the create class

--- GetFit/app/src/main/java/com/example/csanders/getfit/Views/Create.java
+++ GetFit/app/src/main/java/com/example/csanders/getfit/Views/Create.java
@@ -2,6 +2,9 @@ package com.example.csanders.getfit.Views;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 import com.example.csanders.getfit.R;
 
@@ -10,9 +13,28 @@ import com.example.csanders.getfit.R;
  */
 
 public class Create extends Activity {
+    private RadioButton meal;
+    private RadioButton workout;
+    private EditText Name;
+    private EditText Description;
+    private EditText Calories;
+    private EditText Equipment;
+    private Button addItem;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+        meal = (RadioButton)findViewById(R.id.mealSelection);
+        workout = (RadioButton)findViewById(R.id.workoutSelection);
+        Name = (EditText)findViewById(R.id.Name);
+        Description = (EditText)findViewById(R.id.Description);
+        Calories = (EditText)findViewById(R.id.Calories);
+        Equipment = (EditText)findViewById(R.id.Equipment);
+        addItem = (Button)findViewById(R.id.addItemToDB);
+
+
+
     }
 }
