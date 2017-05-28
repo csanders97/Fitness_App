5eb89a7d75c15d961e362a56a5ca43b0ee2a7e86
Caleb Sanders <colonelsanders217@gmail.com>
2017-05-28 14:03:10 -0600

    Still needs more work...

--- GetFit/app/src/main/java/com/example/csanders/getfit/Views/Library.java
+++ GetFit/app/src/main/java/com/example/csanders/getfit/Views/Library.java
@@ -9,44 +9,45 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import com.example.csanders.getfit.Models.Meals;
 import com.example.csanders.getfit.R;
 import com.example.csanders.getfit.Tables.DBHandler;
+
+import java.util.List;
 
 /**
  * Created by Sophie on 5/15/2017.
  */
 
-public class Library extends Activity {
-
+public class Library extends Activity implements View.OnClickListener {
     private Button create;
-    TableLayout tl = (TableLayout) findViewById(R.id.tlTable01);
     DBHandler db = new DBHandler(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library);
-
+        TableLayout tl = (TableLayout) findViewById(R.id.tlTable01);
         create = (Button)findViewById(R.id.createItemBtn);
+        List<Meals> meal = db.getAllMeals();
+        int mealIndex = 0;
+        for(Meals meals : meal) {
+            TableRow row = new TableRow(this);
+            TextView name = new TextView(this);
+            TextView type = new TextView(this);
 
-//        for(int i = 0; i < db.length; i++) {
-//            TableRow row = new TableRow(this);
-//            TextView name = new TextView(this);
-//            TextView type = new TextView(this);
-//
-//            name.setText(db.name);
-//            type.setText(db.type);
-//
-//            row.addView(name);
-//            row.addView(type);
-//            tl.addView(row);
-//        }
+            name.setText(meals.getMealName());
+            type.setText(meals.getMealType());
+
+            row.addView(name);
+            row.addView(type);
+            tl.addView(row);
+        }
 
     }
 
-
-    public void CreateOnClick(View view) {
-
+    @Override
+    public void onClick(View v) {
         Intent create = new Intent(this, Create.class);
         startActivity(create);
     }
