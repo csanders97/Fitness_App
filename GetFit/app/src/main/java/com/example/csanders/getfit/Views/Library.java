80bd4eabedef02ab60a36a3d7c5f5c913aa2bd8e
Caleb Sanders <colonelsanders217@gmail.com>
2017-05-31 12:58:17 -0600

    Working on deleting records

--- GetFit/app/src/main/java/com/example/csanders/getfit/Views/Library.java
+++ GetFit/app/src/main/java/com/example/csanders/getfit/Views/Library.java
@@ -33,73 +33,72 @@ public class Library extends Activity  {
         Bundle bundle = getIntent().getExtras();
         String use = bundle.getString("ID");
         ID = Integer.parseInt(use);
-        //db.addMeals(new Meals(1, "Burrito", 500, 3, "publication", "NAN", "recommendations", "No Restrictions"));
-        //db.addMeals(new Meals(2, "Steak Dinner", 1001, 5, "publication", "NAN", "recommendations", "No Restrictions"));
-        //db.addMeals(new Meals(3, "CheeseBurger", 800, 3, "publication", "NAN", "recommendations", "No Restrictions"));
-        //db.addMeals(new Meals(4, "Hamburger", 200, 3, "publication", "NAN", "recommendations", "No Restrictions"));
-        //db.addWorkouts(new Workouts(1, "Push-Ups", "core", 5));
         TableLayout tl = (TableLayout) findViewById(R.id.tlTable01);
         create = (Button)findViewById(R.id.createItemBtn);
         List<Meals> meal = db.getAllMeals();
         for(Meals meals : meal) {
-            TableRow row = new TableRow(this);
-            TextView name = new TextView(this);
-            TextView type = new TextView(this);
-            Button dlt = new Button(this);
+            if (meals.meals_userID == ID) {
+                TableRow row = new TableRow(this);
+                TextView name = new TextView(this);
+                TextView type = new TextView(this);
+                Button dlt = new Button(this);
 
-            row.setBackgroundColor(Color.argb(1,204,204,204));
-            row.setPadding(0, 0, 1, 1);
+                row.setBackgroundColor(Color.argb(1, 204, 204, 204));
+                row.setPadding(0, 0, 1, 1);
 
-            name.setTextSize(20);
-            name.setText(meals.getMealName());
-            name.setTextSize(22);
-            name.setPadding(40,15,10,15);
-            name.setBackgroundColor(Color.argb(255,255,255,255));
-            type.setText("Meal");
-            type.setTextSize(22);
-            type.setPadding(40,15,10,15);
-            type.setBackgroundColor(Color.argb(255,255,255,255));
-            dlt.setText("Delete");
-            dlt.setTextColor(Color.argb(255,255,255,255));
-            dlt.setBackgroundColor(Color.argb(1,76,175,80));
+                name.setTextSize(20);
+                name.setText(meals.getMealName());
+                name.setTextSize(22);
+                name.setPadding(40, 15, 10, 15);
+                name.setBackgroundColor(Color.argb(255, 255, 255, 255));
+                type.setText("Meal");
+                type.setTextSize(22);
+                type.setPadding(40, 15, 10, 15);
+                type.setBackgroundColor(Color.argb(255, 255, 255, 255));
+                dlt.setText("Delete");
+                dlt.setId(meals.getMealId());
+                dlt.setTextColor(Color.argb(255, 255, 255, 255));
+                dlt.setBackgroundColor(Color.argb(1, 76, 175, 80));
 
 
-            row.addView(name);
-            row.addView(type);
-            row.addView(dlt);
-            tl.addView(row);
+                row.addView(name);
+                row.addView(type);
+                row.addView(dlt);
+                tl.addView(row);
+            }
         }
 
         List<Workouts> workout = db.getAllWorkouts();
         for(Workouts workouts : workout) {
-            Log.i("Workouts: ", workouts.getWorkoutName());
-            TableRow row = new TableRow(this);
-            TextView name = new TextView(this);
-            TextView type = new TextView(this);
-            Button dlt = new Button(this);
+            if (workouts.getWorkout_user_id() == ID) {
+                TableRow row = new TableRow(this);
+                TextView name = new TextView(this);
+                TextView type = new TextView(this);
+                Button dlt = new Button(this);
 
-            row.setBackgroundColor(Color.argb(1,204,204,204));
-            row.setPadding(0, 0, 1, 1);
+                row.setBackgroundColor(Color.argb(1, 204, 204, 204));
+                row.setPadding(0, 0, 1, 1);
 
-            name.setText(workouts.getWorkoutName());
-            name.setTextSize(22);
-            name.setPadding(40,15,10,15);
-            name.setBackgroundColor(Color.argb(255,255,255,255));
-            type.setText("Workout");
-            type.setTextSize(22);
-            type.setPadding(40,15,10,15);
-            type.setBackgroundColor(Color.argb(255,255,255,255));
-            dlt.setText("Delete");
-            dlt.setTextColor(Color.argb(255,255,255,255));
-            dlt.setBackgroundColor(Color.argb(1,76,175,80));
+                name.setText(workouts.getWorkoutName());
+                name.setTextSize(22);
+                name.setPadding(40, 15, 10, 15);
+                name.setBackgroundColor(Color.argb(255, 255, 255, 255));
+                type.setText("Workout");
+                type.setTextSize(22);
+                type.setPadding(40, 15, 10, 15);
+                type.setBackgroundColor(Color.argb(255, 255, 255, 255));
+                dlt.setText("Delete");
+                dlt.setId(workouts.getWorkoutId());
+                dlt.setTextColor(Color.argb(255, 255, 255, 255));
+                dlt.setBackgroundColor(Color.argb(1, 76, 175, 80));
 
 
-            row.addView(name);
-            row.addView(type);
-            row.addView(dlt);
-            tl.addView(row);
+                row.addView(name);
+                row.addView(type);
+                row.addView(dlt);
+                tl.addView(row);
+            }
         }
-
     }
 
     public void CreateOnClick(View view) {
